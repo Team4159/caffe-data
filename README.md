b7d782e4a34076a6dc6867c7e4c5e2ab7e092974
IamTechknow <techhexium@gmail.com>
2017-10-26 20:37:50 -0700

    Update README and update script to be more conveient

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 This repository contains training and test data for team 4159's machine learning project.
 
-## Usage
+## Setup
 
-The data is intended to be used with our reflective tape detector. In our custom caffe repository, clone this one into data, then move the ml folder to data which contains scripts to prepare building the detection model. Rename this one to "VOCdevkit_STEAMWORKS".
+The data is intended to be used with our reflective tape detector. Clone this repository in ~/caffe-data/ml (create if it doesn't exist or clone to ml from caffe-data), then move the ml folder that is inside the repo to $CAFFE\_ROOT/data which contains scripts to prepare building the detection model.
 
+## Creating a custom dataset
+Follow the instructions from the [SSD Caffe Wiki](https://github.com/weiliu89/caffe/wiki/Train-SSD-on-custom-dataset):
+
+* From $CAFFE\_ROOT/data/ml, python gen_trainval.py to create text files for training and testing.
+* Go to $CAFFE\_ROOT then run ./data/ml/create_data.sh to create the LMDB file
+* Now the data is ready to be trained, read the training instructions in our caffe repo to learn more.
--- ml/gen_trainval.py
+++ ml/gen_trainval.py
@@ -2,13 +2,20 @@ import os
 
 """
 Locates the Annotations folder, finds all files 
-and prints the annotation file name with the corresponding image file name.
-The output may be copied and pasted to test.txt and trainval.txt
+and prints the annotation file name with the corresponding image file name
+into trainval.txt and test.txt.
 """
 
-USERNAME = 'edmond'
-base = '/home/' + USERNAME + '/Desktop/caffe/data/VOCdevkit_STEAMWORKS/ml'
-for fname in os.listdir(base + '/Annotations'):
+HOME = '/home/edmond'
+DATASET = 'ml'
+base = HOME + '/data/caffe-data/'
+TEST_FILE = 'test.txt'
+TRAIN_FILE = 'trainval.txt'
+TEST_SIZE = 12
+
+#Get all image names and place onto list
+filenames = []
+for fname in os.listdir(base + DATASET + '/Annotations'):
     if "_out" in fname:
         try:
             idx = fname.index("_out")
@@ -20,6 +27,22 @@ for fname in os.listdir(base + '/Annotations'):
         folder = ''
         img_name = fname.replace(".xml", ".jpg")
 
-    first = 'ml/JPEGImages/' + folder + img_name
-    second = 'ml/Annotations/' + fname
+    first = DATASET + '/JPEGImages/' + folder + img_name
+    second = DATASET + '/Annotations/' + fname
     print(first + ' ' + second)
+    filenames.append(str(first + ' ' + second))
+
+#Split list, add names to files
+trainfiles = filenames[ : len(filenames) - TEST_SIZE - 1]
+testfiles = filenames[len(filenames) - TEST_SIZE : ]
+with open(TRAIN_FILE, "a") as trainfile:
+    for txt in trainfiles:
+        trainfile.write(txt + '\n')
+
+with open(TEST_FILE, "a") as testfile:
+    for txt in testfiles:
+        testfile.write(txt + '\n')
+
+#Create test data image size file
+os.system("{0} {1} {2} {3}".format(os.getenv('CAFFE_ROOT') + '/build/tools/get_image_size', base, TEST_FILE, "test_name_size.txt"))
+
